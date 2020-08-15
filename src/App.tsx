bc5cf9b5f15b3757b368293ef3eaa3e39c17b207
unknown <llaiho666@gmail.com>
2020-08-15 15:15:21 +0300

    more skills

--- src/App.tsx
+++ src/App.tsx
@@ -2,28 +2,32 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import { Button } from "@material-ui/core";
+import Rootview from './views/Rootview';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <p>
-          <Button>testi</Button>
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    // <div className="App">
+    //   <header className="App-header">
+    //     <img src={logo} className="App-logo" alt="logo" />
+    //     <p>
+    //       Edit <code>src/App.tsx</code> and save to reload.
+    //     </p>
+    //     <p>
+    //       <Button>testi</Button>
+          
+    //     </p>
+    //     <a
+    //       className="App-link"
+    //       href="https://reactjs.org"
+    //       target="_blank"
+    //       rel="noopener noreferrer"
+    //     >
+    //       Learn React
+    //     </a>
+    //   </header>
+    // </div>
+    <Rootview />
+    
   );
 }
 
--- src/interfaces/Character.ts
+++ src/interfaces/Character.ts
@@ -2,6 +2,41 @@ import { User } from "./User";
 import { Game } from "./Game";
 // import { v4 } from "uuid";
 
+
+export enum SkillComplexcity {
+    "Simple" = "Simple",
+    "Complex" = "Complex"
+}
+
+export enum SkillTraining {
+    "Basic" = "Basic",
+    "Advanced" = "Advanced"
+}
+
+export enum CharacterAttributes {
+    "STR" = "STR",
+    "BOD" = "BOD",
+    "RFL" = "RFL",
+    "DEX" = "DEX",
+    "INT" = "INT",
+    "WIS" = "WIS",
+    "CHA" = "CHA",
+    "EDG" = "EDG"
+}
+
+
+const skillTargetNumber = new Map<[SkillComplexcity, SkillTraining], number>();
+
+skillTargetNumber.set([SkillComplexcity.Simple, SkillTraining.Basic], 7);
+skillTargetNumber.set([SkillComplexcity.Simple, SkillTraining.Advanced], 8);
+skillTargetNumber.set([SkillComplexcity.Complex, SkillTraining.Basic], 8);
+skillTargetNumber.set([SkillComplexcity.Complex, SkillTraining.Advanced], 9);
+
+export {
+    skillTargetNumber
+}
+
+
 export interface Character {
 
     characterID: string;
@@ -12,15 +47,48 @@ export interface Character {
 
     // attributes
 
-    strenght: number;
-    dexterity: number;
+    
+    str: number;    // strenght
+    bod: number;    // body (constitution)
+    rfl: number;    // reflex
+    dex: number;    // dexterity
+    int: number;    // intelligence
+    wis: number;    // wisdom
+    cha: number;    // charisma
+    edg: number;    // edge (luck)
 
 
     // skills
 
-    administrationTNC: [number, string, string];  // Target Number / skill Compexcity (Simple/Complex)(Basic/Advanced)
-    administration_XP: number;
-    administrationSpecialization: string;
+//    skillAcrobaticsFreeFall_TNC: [number, SkillComplexcity, SkillTraining];     // Target Number / skill Compexcity (Simple/Complex)(Basic/Advanced)
+    skillAcrobatics_TNC: [SkillComplexcity.Simple, SkillTraining.Basic];
+    skillAcrobatics_LinkedAttribute: CharacterAttributes.RFL;
+    skillAcrobaticsFreeFall_XP: number;
+    skillAcrobaticsFreeFall_Specialization: string;
+
+//    skillAcrobaticsGymnastics_TNC: [number, SkillComplexcity, SkillTraining];
+    skillAcrobaticsGymnastics_XP: number;
+    skillAcrobaticsGymnastics_Specialization: string;
+
+    skillActing_TNC: [SkillComplexcity.Complex, SkillTraining.Basic];
+    skillActing_LinkedAttribute: CharacterAttributes.CHA;
+    skillActing_XP: number;
+    skillActing_Specialization: string;
+
+    skillAdministration_TNC: [SkillComplexcity, SkillTraining];  
+    skillAdministration_XP: number;
+    skillAdministration_Specialization: string;
+
+    skillAppraisal_TNC: [SkillComplexcity, SkillTraining];
+    skillAppraisal_XP: number;
+    skillAppraisal_Specialization: string;
+
+    skillArchery_TNC: [SkillComplexcity, SkillTraining];
+    skillArchery_XP: number;
+    skillArchery_Specialization: string;
+
+    skillArt: [SubSkill];
+    skillTiers: boolean;
 
     computersTNC: [number, string, string];
 
@@ -31,13 +99,12 @@ export interface Character {
 }
 
 
-export interface Skill {
+export interface SubSkill {
 
-    skillName: string;
-    skillSubskills: [string];
-    skillTNC: [number, string, string];
-    skillTiers: boolean;
-    skillXP: skillSubskills[number];
+//    skillNameMain: string;
+    skillNameSub: string;
+    skillTNC: [number, SkillComplexcity, SkillTraining];
+    skillXP: number;
 
 }
 
